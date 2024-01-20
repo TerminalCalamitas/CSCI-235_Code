850182538754aa3573455fab4c42b1ea8b3fe9ed
stefexec <stefan.eckel@gmail.com>
2024-01-20 14:38:27 +0100

    fixed a bug where it was not poggers enough

--- testcode.c
+++ testcode.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main()
-{
-	printf("Hello World!\n");
+#define POGGERS int
+#define UWU main
+#define RIGGED printf
+#define UWU_MSG "Hello World!\n"
+
+POGGERS UWU() {
+    RIGGED(UWU_MSG);
+    return 0;
 }
