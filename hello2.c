fee25dd8e48dafb1d3602dd264e3a5d304bfed1c
caleb <paracordcaleb@gmail.com>
2024-01-22 13:48:27 -0500

    some of the work from today

---
+++ hello2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	// this is a single line comment
+	/*
+	* This is a multi
+	* line comment
+	*/
+	
+	int x;
+	x = 5;
+	int y = 10;
+	double z = 3.141592;
+	printf("Hello, the value of x is %d and the value of z is %f\n", x, z);
+
+
+}
---
+++ hello3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define VALUE 55
+
+int main()
+{
+	int x = VALUE;
+	while (x> 0) 
+	{
+		printf("X is %d\n",x);
+		x--;
+	}
+	
+}
